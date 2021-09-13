b37cdb4a8a1efd01613311b1290a9fc166eec743
Diskaoktavaia <diskaoktavia36@gmail.com>
2021-09-13 13:24:52 +0700

    membuat HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\HomeController;
 
 use Illuminate\Http\Request;
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 });
 
 Route::get('/', [HomeController::class, 'index']);
+
